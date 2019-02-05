9607668eb4b32dda8a9b9d2d4a00672c3bfe8cc0
KrishanRj <30726173+KrishanRj@users.noreply.github.com>
2019-02-05 21:25:39 +0530

    test

---
+++ front_end/src/app/components/create/create.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,Validators} from "@angular/forms";
+import { Router} from "@angular/router";
+import { IssueService} from "../../issue.service";
+import { FormBuilder } from "@angular/forms";
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  createForm: FormGroup;
+
+  constructor(private issueService: IssueService, private router: Router, private fb: FormBuilder) {
+
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      responsible: '',
+      description: '',
+      severity: ''
+    });
+  }
+
+  addIssue(title, responsible, description, severity){
+    this.issueService.addIssue(title, responsible, description, severity).subscribe(() => {
+      this.router.navigate([`/list`]);
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+}
